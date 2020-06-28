6b47b9ac225c0641cf19878ee39596d26719c57e
acheao <acheao@foxmail.com>
2020-06-28 19:47:07 +0800

    添加类

--- src/main/java/spitter/web/HomeController.java
+++ src/main/java/spitter/web/HomeController.java
@@ -6,11 +6,15 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @Controller
 //@RequestMapping(value = "/ttt")
 public class HomeController {
+
     @Autowired
     StringRedisTemplate redis;
 
@@ -21,7 +25,9 @@ public class HomeController {
     public String home(){
         System.out.println("----------------");
         redis.opsForValue().set("20200606", "acheao");
+        redis.opsForValue().append("20200606","20200607");
 //        redisConnectionFactory.getConnection().
+        Map map =  new HashMap();
         return "home";
     }
 }
